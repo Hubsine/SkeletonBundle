609536424c15295295157ab10eead4df141b972d
Hubsine <contact@hubsine.com>
2019-08-22 01:57:50 +0200

    Install and config EasyAdmin.

---
+++ src/HubsineSkeletonBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * HubsineSkeletonBundle
+ *
+ * @author Hubsine <contact@hubsine.com>
+ */
+class HubsineSkeletonBundle extends Bundle
+{
+    
+}
