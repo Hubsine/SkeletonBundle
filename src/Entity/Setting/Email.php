991c284d129be1c497d045ce4c2cb4488b42a4ae
Hubsine <contact@hubsine.com>
2019-09-04 23:12:58 +0200

    add support for email config

---
+++ src/Entity/Setting/Email.php
@@ -0,0 +1,216 @@
+<?php
+
+namespace Hubsine\SkeletonBundle\Entity\Setting;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Hubsine\SkeletonBundle\Validator\Constraints\UniqueEntry;
+
+/**
+ * 
+ * Email 
+ * 
+ * @ORM\Entity(repositoryClass="Hubsine\SkeletonBundle\Repository\Setting\EmailRepository")
+ * 
+ * @UniqueEntry(groups={"create"})
+ */
+class Email
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * 
+     * @Assert\NotBlank()
+     * @Assert\Email(checkMX=true, checkHost=true)
+     */
+    private $fromEmail;
+
+    /**
+     * @ORM\Column(type="string", length=155)
+     * 
+     * @Assert\NotBlank()
+     * @Assert\Type(type="string")
+     */
+    private $fromName;
+
+    /**
+     * @ORM\Column(type="string", length=155)
+     * 
+     * @Assert\NotBlank()
+     * @Assert\Email(checkMX=true, checkHost=true)
+     */
+    private $responseEmail;
+
+    /**
+     * @ORM\Column(type="string", length=155)
+     * 
+     * @Assert\Type(type="string")
+     */
+    private $host;
+
+    /**
+     * @ORM\Column(type="integer")
+     * 
+     * @Assert\NotBlank()
+     * @Assert\Type(type="integer")
+     */
+    private $port;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * 
+     * @Assert\NotBlank()
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * 
+     * @Assert\NotBlank()
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="string", length=10)
+     * 
+     * @Assert\choice(callback="getEncryptions")
+     */
+    private $encryption;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     * 
+     * @Assert\choice(callback="getAuthModes")
+     */
+    private $authMode;
+    
+    
+    public static function getEncryptions()
+    {
+        return array('tls', 'ssl');
+    }
+    
+    public static function getAuthModes()
+    {
+        return array('plain', 'login', 'cram-md5');
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFromEmail(): ?string
+    {
+        return $this->fromEmail;
+    }
+
+    public function setFromEmail(string $fromEmail): self
+    {
+        $this->fromEmail = $fromEmail;
+
+        return $this;
+    }
+
+    public function getFromName(): ?string
+    {
+        return $this->fromName;
+    }
+
+    public function setFromName(string $fromName): self
+    {
+        $this->fromName = $fromName;
+
+        return $this;
+    }
+
+    public function getResponseEmail(): ?string
+    {
+        return $this->responseEmail;
+    }
+
+    public function setResponseEmail(string $responseEmail): self
+    {
+        $this->responseEmail = $responseEmail;
+
+        return $this;
+    }
+
+    public function getHost(): ?string
+    {
+        return $this->host;
+    }
+
+    public function setHost(string $host): self
+    {
+        $this->host = $host;
+
+        return $this;
+    }
+
+    public function getPort(): ?int
+    {
+        return $this->port;
+    }
+
+    public function setPort(int $port): self
+    {
+        $this->port = $port;
+
+        return $this;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getEncryption(): ?string
+    {
+        return $this->encryption;
+    }
+
+    public function setEncryption(string $encryption): self
+    {
+        $this->encryption = $encryption;
+
+        return $this;
+    }
+
+    public function getAuthMode(): ?string
+    {
+        return $this->authMode;
+    }
+
+    public function setAuthMode(string $authMode): self
+    {
+        $this->authMode = $authMode;
+
+        return $this;
+    }
+}
---
+++ src/Form/Setting/SettingAuthType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Hubsine\SkeletonBundle\Form\Setting;
+
+use Hubsine\SkeletonBundle\Entity\Setting\Email;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SettingAuthType extends ChoiceType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $options['choices']  = Email::getAuthModes();
+        parent::buildForm($builder, $options);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        parent::configureOptions($resolver);
+        
+        $resolver->setDefaults(
+            [
+                'choice_label'  => function($value, $key, $index)
+                {
+                    return $value;
+                }
+            ]
+        );
+    }
+}
