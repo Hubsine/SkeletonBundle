76faa3c69e521477e9a94ac401da226560fae147
Hubsine <contact@hubsine.com>
2019-09-14 19:03:21 +0200

    conditional constraint unique entity

--- src/Entity/Content/Page.php
+++ src/Entity/Content/Page.php
@@ -5,18 +5,16 @@ namespace Hubsine\SkeletonBundle\Entity\Content;
 use Doctrine\ORM\Mapping as ORM;
 use Knp\DoctrineBehaviors\Model as ORMBehaviors;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
-use Hubsine\SkeletonBundle\Validator\Constraints\UniqueEntry;
 use Hubsine\SkeletonBundle\Traits\Entity\MagicGetTrait;
 
 /**
  * @ORM\Entity(repositoryClass="Hubsine\SkeletonBundle\Repository\Content\PageRepository")
- * 
- * @UniqueEntity("tag")
  */
 class Page
 {
-    const AVAIBLES_TAGS = ['a-propos', 'contact', 'cgu', 'cgv', 'mentions-legales', 'points-distribution'];
+    const AVAIBLES_TAGS = ['a-propos', 'contact', 'cgu', 'cgv', 'mentions-legales', 'autre'];
     
     use ORMBehaviors\Translatable\Translatable;
     use MagicGetTrait;
@@ -57,4 +55,26 @@ class Page
 
         return $this;
     }
+    
+    /**
+     * @Assert\Callback
+     */
+    public function validate(ExecutionContextInterface $context, $payload)
+    {
+        if( $this->tag !== 'autre' )
+        {
+            $uniqueEntity = new UniqueEntity(['fields'  => 'tag']);
+            $uniqueEntity->entityClass = self::class;
+            
+            $errors = $context->getValidator()->validate($this, $uniqueEntity);
+            
+            if( $errors->count() > 0 )
+            {
+                $context->buildViolation($uniqueEntity->message)
+                        ->atPath('tag')
+                        ->addViolation();
+            }
+        }
+    }
+    
 }
