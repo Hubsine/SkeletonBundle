264b76a9fd36beda4828cb0bf820e47934f5dc3c
Hubsine <contact@hubsine.com>
2019-09-26 23:10:33 +0200

    Test class SkeletonVariable

---
+++ tests/Twig/SkeletonVariableTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Hubsine\SkeletonBundle\Tests\Twig;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Hubsine\SkeletonBundle\Entity\Appearance as AppearanceEntity;
+
+/**
+ * SkeletonVariable
+ *
+ * @author Hubsine <contact@hubsine.com>
+ */
+class SkeletonVariableTest extends WebTestCase
+{
+    public function testBuild()
+    {
+        $kernel             = self::bootKernel();
+        $container          = $kernel->getContainer();
+        $skeletonVariable   = $container->get('hubsine.skeleton.variable');
+        
+        // Defaults values
+        $this->assertInstanceOf(AppearanceEntity\SiteTranslation::class, $site = $skeletonVariable->site);
+        $this->assertInstanceOf(AppearanceEntity\Logo::class, $logo = $skeletonVariable->logo);
+        $this->assertIsArray($socialNetwork = $skeletonVariable->socialNetwork);
+        
+        // Variable doest exist
+        $this->assertNull($skeletonVariable->teszefhezf);
+        
+        
+        // Add and Remove assertion
+        $skeletonVariable->addVariable('Foo', 'Foo');
+        $this->assertEquals('Foo', $skeletonVariable->Foo);
+        
+        $skeletonVariable->removeVariable('Foo');
+        $this->assertNull($skeletonVariable->Foo);
+    }
+}
