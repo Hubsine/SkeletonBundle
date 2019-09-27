a95dd415a8f096e98077cfef072b9f6382ef67c3
Hubsine <contact@hubsine.com>
2019-09-27 23:49:09 +0200

    send contact message with new service definition

--- src/Controller/Contact/FormController.php
+++ src/Controller/Contact/FormController.php
@@ -6,12 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Hubsine\SkeletonBundle\Entity\Contact\Form;
 use Hubsine\SkeletonBundle\Form\Contact\FormType;
-use Hubsine\SkeletonBundle\Entity\Setting\Email;
 
 class FormController extends Controller
 {
-    /**
-     */
     public function indexAction(Request $request)
     {
         $formEntity = new Form();
@@ -19,51 +16,35 @@ class FormController extends Controller
         
         
         $form->handleRequest($request);
-        
+       
         if( $form->isSubmitted() && $form->isValid() )
         {
-            $emailConfig = $this->getDoctrine()
-                    ->getRepository(Email::class)
-                    ->findOneBy([]);
+            $mailer     = $this->get('swiftmailer.mailer.skeleton');
+            $message    = $mailer->getMessageWithSenderConfig();
             
-            if( $emailConfig instanceof Email )
+            $message->setReplyTo( $formEntity->getEmail(), $formEntity->getFirstName() . ' ' . $formEntity->getLastName() )
+                    ->setBody(
+                        $this->renderView(
+                            '@HubsineSkeleton/contact/message.html.twig',
+                            ['message' => $formEntity]
+                        ),
+                        'text/html'
+                    );
+            
+            $send = $mailer->send( $message );
+            
+            if(  $send === 0 )
             {
-                $mailer = new \Swift_Mailer( $this->getTransport($emailConfig) );
-                $mailer->send( $this->getMessage($formEntity, $emailConfig) );
+                $this->addFlash('dander', 'Your message can not be sent.');
+            }else{
+                $this->addFlash('success', 'Your message is sent.');
             }
+            
+            return $this->redirectToRoute('page_contact');
         }
         
         return $this->render('@HubsineSkeleton/contact/index.html.twig', [
             'form'  => $form->createView()
         ]);
-    }
-    
-    private function getMessage(Form $formEntity, Email $emailConfig)
-    {
-        return $message = (new \Swift_Message())
-            ->setFrom($emailConfig->getFromEmail())
-            ->setTo($formEntity->getEmail())
-            ->setSubject('Message from Ethno Tendance')    
-            ->setReplyTo($formEntity->getEmail(), $formEntity->getFirstName() . ' ' . $formEntity->getLastName() )
-            ->setBody(
-                $this->renderView(
-                    '@HubsineSkeleton/contact/message.html.twig',
-                    ['message' => $formEntity]
-                ),
-                'text/html'
-            )
-        ;
-    }
-    
-    private function getTransport(Email $emailConfig)
-    {
-        return $transport = ( new \Swift_SmtpTransport())
-                ->setHost($emailConfig->getHost())
-                ->setUsername($emailConfig->getUsername())
-                ->setPassword($emailConfig->getPassword())
-                ->setPort($emailConfig->getPort())
-                ->setEncryption($emailConfig->getEncryption())
-                ->setAuthMode($emailConfig->getAuthMode())
-                ;
     }
 }
