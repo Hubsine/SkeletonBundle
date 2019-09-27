9bb1790534afb75aa56c9078c8a4316d65a234dd
Hubsine <contact@hubsine.com>
2019-09-27 23:50:37 +0200

    update assert with new class

---
+++ tests/Mailer/SkeletonMailerTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Hubsine\SkeletonBundle\Tests\Mailer;
+
+use Hubsine\SkeletonBundle\Test\AbstractWebTestCase;
+use Hubsine\SkeletonBundle\Entity\Setting\Email;
+use Hubsine\SkeletonBundle\Entity\Setting\EmailTransport;
+
+/**
+ * SkeletonMailer
+ *
+ * @author Hubsine <contact@hubsine.com>
+ */
+class SkeletonMailer extends AbstractWebTestCase
+{
+    public function testSendEmail()
+    {
+        $mailer = static::$container->get('hubsine.skeleton.mailer');
+        
+        $this->assertInstanceOf(Email::class, $mailer->getSenderConfig());
+        $this->assertInstanceOf(EmailTransport::class, $mailer->getTransportConfig());
+        
+    }
+}
