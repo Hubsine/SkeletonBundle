08e59391ab0e4c76076bff567b38cc6b74536d26
Hubsine <contact@hubsine.com>
2019-10-18 23:38:50 +0200

    purge abstract class content

--- src/Test/AbstractWebTestCase.php
+++ src/Test/AbstractWebTestCase.php
@@ -11,17 +11,4 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
  */
 abstract class AbstractWebTestCase extends WebTestCase
 {
-    /**
-     *
-     * @var Symfony\Component\DependencyInjection\ContainerInterface|null
-     */
-    protected static $container;
-    
-    public function __construct($name = null, $data = array(), $dataName = '') 
-    {
-        parent::__construct($name, $data, $dataName);
-        
-        static::$kernel = self::bootKernel();
-        static::$container = static::$kernel->getContainer();
-    }
 }
